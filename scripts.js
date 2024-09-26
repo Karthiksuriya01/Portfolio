6424e265b00cf37967ab904a6916e39099dc7c33
Karthiksuriya01 <karthiksuriy01@gmail.com>
2024-09-27 02:06:45 +0530

    Updated

--- scripts.js
+++
@@ -1,7 +0,0 @@
-// Interactive Form Validation
-const form = document.getElementById('contact-form');
-
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Message Sent! I will contact you soon.');
-});
